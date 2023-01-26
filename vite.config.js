2cf96c4ca15c6bce1aa973c3896a80100f35adac
mmskazak <mmskazak@gmail.com>
2023-01-26 17:00:53 +0300

    upd

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/sass/main.sass',
+            'resources/js/app.js',
+        ]),
+    ],
+});
+
