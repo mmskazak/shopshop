28eb809f5bcacc14d7becd11cc37f7e943823158
mikhail <mmskazak@gmail.com>
2023-01-29 20:35:16 +0300

    fix

--- app/Logging/Telegram/TelegramLoggerFactory.php
+++ app/Logging/Telegram/TelegramLoggerFactory.php
@@ -6,10 +6,10 @@ use Monolog\Logger;
 
 class TelegramLoggerFactory {
 
-    public function __invoke(string $level, string $token, string $chatId):Logger
+    public function __invoke(array $config): Logger
     {
         $logger = new Logger('telegram');
-        $logger->pushHandler(new TelegramLoggerHandler($level, $token, $chatId));
+        $logger->pushHandler(new TelegramLoggerHandler($config['level'], $config['token'], $config['chat_id']));
         return $logger;
     }
 
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    logger()->channel('telegram')->debug('Hello, world');
+    logger()->channel('telegram')->debug('Hello world!');
     return view('welcome');
 });
