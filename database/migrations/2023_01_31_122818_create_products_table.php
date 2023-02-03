005fd9878ddaf28fd3ca5c7fe44384bf26b98159
mmskazak <mmskazak@gmail.com>
2023-02-03 15:01:21 +0300

    fixes +

--- database/migrations/2023_01_31_122818_create_products_table.php
+++ database/migrations/2023_01_31_122818_create_products_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
                 ->nullable()
                 ->constrained()
                 ->cascadeOnUpdate()
-                ->onDelete('set null');
+                ->nullOnDelete();
             $table->timestamps();
         });
 
